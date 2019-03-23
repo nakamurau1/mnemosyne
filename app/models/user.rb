7e13b2b4e9498a5be194327771067ec57113b053
Yuichi Nakamura <fyucihi@gmail.com>
2019-03-23 17:30:01 +0900

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,31 @@ class User < ApplicationRecord
     validates :password, presence: true, length: {minimum: 6}
     has_secure_password
 
+    attr_accessor :remember_token
+
   # 渡された文字列のハッシュ値を返す
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # ランダムなトークンを返す
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.blank?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 end
