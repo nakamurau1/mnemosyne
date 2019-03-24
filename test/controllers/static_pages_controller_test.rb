c5af2d6e1fa8d988d46745bd9628b50deab853ea
Yuichi Nakamura <fyucihi@gmail.com>
2019-03-24 16:52:57 +0900

    View周りを日本語化

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,24 +8,24 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "TOP | #{@base_title}"
   end
 
   test "should get help" do
     get help_path 
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select "title", "ヘルプ | #{@base_title}"
   end
 
   test "should get about" do
     get about_path 
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", "本サービスについて | #{@base_title}"
   end
 
   test "should get contact" do
     get contact_path 
     assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
+    assert_select "title", "連絡先 | #{@base_title}"
   end
 end
