32d2b3c39ed75096f6f5ec60a92c04c63c347ea4
Yuichi Nakamura <fyucihi@gmail.com>
2019-03-27 19:52:05 +0900

    Itemにuser_idフィールドを追加

--- db/migrate/20190326102544_create_items.rb
+++ db/migrate/20190326102544_create_items.rb
@@ -3,6 +3,7 @@ class CreateItems < ActiveRecord::Migration[5.1]
     create_table :items do |t|
       t.text :front_text
       t.text :back_text
+      t.references :user, foreign_key: true, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,10 @@ ActiveRecord::Schema.define(version: 20190326102544) do
   create_table "items", force: :cascade do |t|
     t.text "front_text"
     t.text "back_text"
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
