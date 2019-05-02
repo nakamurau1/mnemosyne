77d2bc3d91a631bcc5423ca2ce6cf3aaf30a6a06
Yuichi Nakamura <nakamurau1@nakamurayuuichi-no-MacBook-Pro.local>
2019-05-02 18:58:18 +0900

    暗記アイテムの登録・編集画面にデッキを選択する項目を追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -54,7 +54,7 @@ class ItemsController < ApplicationController
 private
 
     def _item_params
-      params.require(:item).permit(:front_text, :back_text, :front_picture, :back_picture)
+      params.require(:item).permit(:front_text, :back_text, :front_picture, :back_picture, :deck_id)
     end
 
 end
