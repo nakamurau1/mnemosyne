441959a5eab94626130fd8ed9ec59ad4606a4781
Yuichi Nakamura <nakamurau1@nakamurayuuichi-no-MacBook-Pro.local>
2019-05-02 20:54:25 +0900

    デッキの表示順を固定

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -2,6 +2,7 @@ class Deck < ApplicationRecord
   after_initialize :_set_default_value, if: :new_record?
   belongs_to :user
   has_many :items, dependent: :destroy
+  default_scope { order(id: :desc) }
 
   def public_str
     if public?
